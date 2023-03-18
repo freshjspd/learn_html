eccfff09ab6d08c788a68a93337b0b0ab6b1ac4b
freshjspd <freshjs.pd@gmail.com>
2023-03-18 13:21:21 +0200

    dom. usercard. structure with function

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,54 +8,61 @@ const user = {
     }
 };
 
+function createUserPhoto(userImgSrc){
+    const imgContainer = document.createElement('div');
+    imgContainer.classList.add('image_container');
+    const imgEl = document.createElement('img');
+    imgEl.setAttribute('src', userImgSrc);
+    imgEl.setAttribute('alt', 'photo');
+    imgContainer.append(imgEl);
+    return imgContainer;
+}
+
+function createBtnContainer(stats){
+    const buttonContainer = document.createElement('div');
+    buttonContainer.classList.add('button_container');
+
+    const statEl1 = document.createElement('div');
+    statEl1.classList.add('stat', 'fb');
+    statEl1.innerText = stats.fb;
+
+    const statEl2 = document.createElement('div');
+    statEl2.classList.add('stat', 'insta');
+    statEl2.innerText = stats.insta;
+
+    const statEl3 = document.createElement('div');
+    statEl3.classList.add('stat', 'tel');
+    statEl3.innerText = stats.tel;
+
+    buttonContainer.append(statEl1);
+    buttonContainer.append(statEl2);
+    buttonContainer.append(statEl3);
+    return buttonContainer;
+
+}
+
+function createUserFullName(fullName){
+    const userFullName = document.createElement('p');
+    userFullName.classList.add('user_fullname');
+    userFullName.innerText = fullName;
+    return userFullName;
+}
+
 
 const userCardsEl = document.getElementById('userCards');
 
 const card = document.createElement('div');
 card.classList.add('card_container');
 
-//img
-const imgContainer = document.createElement('div');
-imgContainer.classList.add('image_container');
-
-const imgEl = document.createElement('img');
-imgEl.setAttribute('src', user.img);
-imgEl.setAttribute('alt', 'photo');
-
-imgContainer.append(imgEl);
+const imgContainer = createUserPhoto(user.img);
 card.append(imgContainer);
-
-//info
 
 const infoContainer = document.createElement('div');
 infoContainer.classList.add('info_container');
-
-const userFullName = document.createElement('p');
-userFullName.classList.add('user_fullname');
-userFullName.innerText = user.fullName;
+const userFullName = createUserFullName(user.fullName);
 infoContainer.append(userFullName);
-
-const buttonContainer = document.createElement('div');
-buttonContainer.classList.add('button_container');
-
-const statEl1 = document.createElement('div');
-statEl1.classList.add('stat', 'fb');
-statEl1.innerText = user.stats.fb;
-
-const statEl2 = document.createElement('div');
-statEl2.classList.add('stat', 'insta');
-statEl2.innerText = user.stats.insta;
-
-const statEl3 = document.createElement('div');
-statEl3.classList.add('stat', 'tel');
-statEl3.innerText = user.stats.tel;
-
-buttonContainer.append(statEl1);
-buttonContainer.append(statEl2);
-buttonContainer.append(statEl3);
-
+const buttonContainer = createBtnContainer(user.stats);
 infoContainer.append(buttonContainer);
-
 card.append(infoContainer);
 
 // card => body
