906b59f93751390c029f80786de57e8201eb0e6a
freshjspd <freshjs.pd@gmail.com>
2023-03-18 13:40:53 +0200

    dom. list of usercards

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
-const user = {
+const users = [
+{
     img: 'https://cdn.dribbble.com/users/2131993/screenshots/15628402/media/7bb0d27e44d8c2eff47276ae86bfd6a3.png?compress=1&resize=400x300',
     fullName: 'Kate Adson',
     stats: {
@@ -6,7 +7,28 @@ const user = {
         insta: '1000',
         tel: '950'
     }
-};
+},
+{
+    img: 'https://t4.ftcdn.net/jpg/03/69/19/81/360_F_369198116_K0sFy2gRTo1lmIf5jVGeQmaIEibjC3NN.jpg',
+    fullName: 'John Fox',
+    stats: {
+        fb: '600',
+        insta: '800',
+        tel: '780'
+    }
+},
+{
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSF8Vn1rYx1PsJLDNV7-1U4NFUKo7AfxWaB3A&usqp=CAU',
+    fullName: 'Tom Tomson',
+    stats: {
+        fb: '1800',
+        insta: '1200',
+        tel: '950'
+    }
+},
+];
+
+
 
 function createUserPhoto(userImgSrc){
     const imgContainer = document.createElement('div');
@@ -49,22 +71,32 @@ function createUserFullName(fullName){
 }
 
 
+function createCard(user){
+    const card = document.createElement('div');
+    card.classList.add('card_container');
+
+    const imgContainer = createUserPhoto(user.img);
+    card.append(imgContainer);
+
+    const infoContainer = document.createElement('div');
+    infoContainer.classList.add('info_container');
+    const userFullName = createUserFullName(user.fullName);
+    infoContainer.append(userFullName);
+    const buttonContainer = createBtnContainer(user.stats);
+    infoContainer.append(buttonContainer);
+    card.append(infoContainer);
+
+    return card;
+}
+
+
 const userCardsEl = document.getElementById('userCards');
+//const cards = users.map( u => userCardsEl.append(document.createElement('li').append(createCard(u))));
 
-const card = document.createElement('div');
-card.classList.add('card_container');
+const cards = users.map( u => {
+    let liCard = document.createElement('li');
+    let card = liCard.append(createCard(u));
+    userCardsEl.append(liCard);
+});
 
-const imgContainer = createUserPhoto(user.img);
-card.append(imgContainer);
-
-const infoContainer = document.createElement('div');
-infoContainer.classList.add('info_container');
-const userFullName = createUserFullName(user.fullName);
-infoContainer.append(userFullName);
-const buttonContainer = createBtnContainer(user.stats);
-infoContainer.append(buttonContainer);
-card.append(infoContainer);
-
-// card => body
-userCardsEl.append(card);
 
