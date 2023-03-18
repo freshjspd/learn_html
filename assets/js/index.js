78a9510f3aacbdfd2192dabfe18263139f128859
freshjspd <freshjs.pd@gmail.com>
2023-03-18 10:33:31 +0200

    dom. get elements

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,46 @@
-let elemEl = document.getElementById('elem');
-console.log(elemEl);
-elemEl.style.background = "blue";
+//let containerEl = document.getElementById('container');
+let containerEl = 
+document.querySelector('#container');
+containerEl.style.backgroundColor = 'yellow';
 
-let childEl = document.getElementById('childElem');
-childEl.style.background = "red";
+
+//let redTextEl = document.querySelector('.redText');
+
+let redTextEl = 
+document.querySelector('div#container > p.redText');
+redTextEl.style.color = 'red';
+redTextEl.style.fontSize = '20px';
+redTextEl.style.fontWeight = 'bold';
+
+let textMarkElems = 
+document.querySelectorAll('p.mark');
+console.log(textMarkElems);
+console.log('-----------------');
+
+textMarkElems.forEach(el => {
+    el.style.backgroundColor = 'chartreuse';
+    el.style.color = 'white';
+    el.style.fontWeight = 'bold';
+    el.style.fontSize = '18px';
+    console.log(el.classList);
+    console.log(el.attributes);
+});
+
+//let textMarkEl2 =
+//document.getElementsByClassName('mark'); 
+//console.log(textMarkEl2);
+
+
+/*
+Получение элементов:
+getElementById('idName')
+getElementsByName('class')
+getElementsByClassName('class')
+getElementsByTagName('tag')
+
+querySelector('cssSelector')
+.class  #id 
+
+querySelectorAll('cssSelector')
+
+*/
