19781c1189bd213258bb478e2a1f5f28b968aff3
freshjspd <freshjs.pd@gmail.com>
2023-03-18 09:56:18 +0200

    dom intro

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+let elemEl = document.getElementById('elem');
+console.log(elemEl);
+elemEl.style.background = "blue";
+
+let childEl = document.getElementById('childElem');
+childEl.style.background = "red";
