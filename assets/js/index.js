9fb7256f875231398f37b4df2e88af7d7f4085ca
freshjspd <freshjs.pd@gmail.com>
2023-03-19 11:31:27 +0200

    box list. change classes

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,15 +3,15 @@ const boxes = document.querySelectorAll('.box');
 boxes.forEach(b => b.addEventListener('click', makeColor1));
 
 function makeColor1(event){
-    //console.log(event.target);
-    //console.log(this);
-    this.style.backgroundColor = 'black';
+    this.classList.remove('boxStyle2');
+    this.classList.add('boxStyle1');
     this.removeEventListener('click', makeColor1);
     this.addEventListener('click', makeColor2);
 }
 
 function makeColor2(event){
-    this.style.backgroundColor = 'white';
+    this.classList.remove('boxStyle1');
+    this.classList.add('boxStyle2');
     this.removeEventListener('click', makeColor2);
     this.addEventListener('click', makeColor1);
 }
