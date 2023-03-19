6ab910b25519b755bd88275a706fc1c2c0814d40
freshjspd <freshjs.pd@gmail.com>
2023-03-19 10:06:08 +0200

    easy saper. style

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,20 @@
 const board = document.getElementById("board");
 const count = 100;
+const colors = ['greenyellow', 'green', 'red', 'yellow', 'blue', 'cyan', 'fuchsia', 'magenta'];
+
 for(let i=0; i<count; i++){
     const square = document.createElement('div');
     square.classList.add('square');
     board.append(square);
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = getRandomColor(colors);
+    });
+    square.addEventListener('mouseleave', () => {
+        square.style.backgroundColor = 'gray';
+    });
+}
+
+function getRandomColor(arrayColors){
+    const index = Math.floor(Math.random() * arrayColors.length);
+    return arrayColors[index];
 }
