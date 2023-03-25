936f00e65306c900a9c2172b21bb521f4931e74b
freshjspd <freshjs.pd@gmail.com>
2023-03-25 13:26:42 +0200

    structure: generator

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,67 +1,16 @@
-class Item{
-    constructor(value, next = null, prev = null){
-        this.value = value;
-        this.next = next;
-        this.prev = prev;
-    }
+function* generator1(){
+    yield 1;
+    yield 2;
+    yield 3;
+    yield 4;
 }
 
-class List{
-    constructor(){
-        this.head = null;
-        this.tail = null;
-        this.size = 0;
-    }
-    addToStart(value){
-        const newHead = new Item(value, this.head, null);
-        if(this.head){ this.head.prev = newHead; }
-        this.head = newHead;
-        if(!this.tail) { this.tail = newHead; }
-        this.size ++;
-    }
-    addToEnd(value){
-        const newTail = new Item(value, null, this.tail);
-        if(this.tail){ this.tail.next = newTail;}
-        this.tail = newTail;
-        if(!this.head){ this.head = newTail;}
-        this.size++;
-    }
-    addAfterNode(node, value){
-        if(this.tail == node){ this.addToEnd(value); return;}
-        const newItem = new Item(value, node.next, node);
-        node.next = newItem;
-        newItem.next.prev = node;
-        this.size++;
-    }
-    [Symbol.iterator](){
-        let el = this.head;
-        return{
-            next(){
-                if(el){
-                    const value = el.value;
-                    el = el.next;
-                    return{
-                        value: value,
-                        done: false,
-                    }
-                } else{
-                    return{
-                        done: true,
-                    }
-                }
-            }
-        }
-    }
-}
+const player1 = generator1();
+const player2 = generator1();
 
-let list1 = new List();
-list1.addToStart(5);
-list1.addToStart('hello');
-list1.addToStart({name:'Masha'});
-list1.addToStart(100);
+player1.next(); // 1
+player1.next(); // 2
+player2.next(); // 3
 
-/*
-for(let i of list1){
-    console.log(i);
-}
-*/
+player2.next(); // 1
+
