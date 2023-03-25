524c388127bf37a290bf34f04eedd6c4498d83b2
freshjspd <freshjs.pd@gmail.com>
2023-03-25 12:04:27 +0200

    location

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,15 @@
-const[backEl,forwardEl] = document.querySelectorAll('button');
-backEl.onclick = () => history.back();
-forwardEl.onclick = () => history.forward();
+const cont = document.querySelector('button');
+cont.onclick = function(){
+    location.assign(`${location.origin}/assets/pages/contacts.html`);
+}
+
+//location
+// origin - index.html обращение к корню
+// pathname - путь к текущей страницы
+// search - параметры строки запроса
+// href - полная ссылка (адрес)
+
+// методы 
+// reload()  - перезагрузка страницы
+// replace() - старая страница не добавляется
+// assign() - добавится новая страница
