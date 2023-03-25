f7d73e58269022ae057f53b3d8b1303fedb6724e
freshjspd <freshjs.pd@gmail.com>
2023-03-25 11:50:48 +0200

    history

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,3 @@
-const ranges = document.querySelectorAll('input[type="range"]');
-ranges.forEach(el => el.oninput = updBodyColor);
-
-function updBodyColor(event){
-    let color = "rgba(";
-    ranges.forEach((el, index, list) =>
-        index == list.length -1 ? 
-            color += el.value + ')':
-            color += el.value + ','
-    );
-    document.body.style.backgroundColor = color;
-    localStorage.setItem('DOC_BG_COLOR', color);
-}
-
-window.addEventListener('load', (event) => {
-    const gettedBGColor = localStorage.getItem('DOC_BG_COLOR');
-    document.body.style.backgroundColor = gettedBGColor;
-});
-
+const[backEl,forwardEl] = document.querySelectorAll('button');
+backEl.onclick = () => history.back();
+forwardEl.onclick = () => history.forward();
