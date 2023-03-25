7345974b1c5c36df05816a5fdca4fec08324bbf0
freshjspd <freshjs.pd@gmail.com>
2023-03-25 09:40:55 +0200

    window screen - size

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,39 +1,13 @@
-const IMGS = [
-    'https://kaboompics.com/cache/d/9/a/9/5/d9a95053e9df780b5922013e4840b88769687f6c.jpeg', 
-    'https://kaboompics.com/cache/6/b/3/8/d/6b38d175fac8e9fcfdfa438ac0e52e063278ec12.jpeg', 
-    'https://kaboompics.com/cache/b/7/3/0/d/b730dbb76e707683aa973c77c2e79e9c0caac420.jpeg', 
-    'https://kaboompics.com/cache/9/f/f/d/2/9ffd225edb872ae0e21302ea924955dd7098f40d.jpeg'
-];
+const [widthElem, heightElem] = document.querySelectorAll('h1');
 
-const COLORS = [
-    ['#7D200F', '#A83520'],
-    ['#A5826F','#300C0E'],
-    ['#66A6D0','#00489A'],
-    ['#87BFD0','#01507B']
-];
+window.addEventListener("resize", fun);
+window.addEventListener("load", fun);
 
-const container = document.querySelector('.container');
-const sidebar = document.querySelector('.sidebar');
-const mainbar = document.querySelector('.mainbar');
-const controls = document.querySelector('.controls');
-const colorBoxes = document.querySelectorAll('.box');
-const imgBoxes = document.querySelectorAll('.img-box');
-const upButton = document.querySelector ('.up-button');
-const downButton = document.querySelector ('.down-button');
+//window.onresize = fun;
 
-const slidesCount = 4;
-let activeSlideIndex = 1;
+function fun(event){
 
-colorBoxes.forEach( (i, index) => {
-    console.log('index ', index);
-    const indexColor = slidesCount - index - 1;
-    console.log('index color ', indexColor);
-    i.style.background = `linear-gradient(to left,${COLORS[indexColor][0]}, ${COLORS[indexColor][1]})`;
-});
-
-imgBoxes.forEach( (i, index) => {
-    i.style.backgroundImage = `url(${IMGS[index]})`;
-});
-
-
-
+    //window.innerWidth - внутреняя ширина браузера (не вьюпорт)
+    widthElem.innerText = `width=${window.outerWidth}px`;
+    heightElem.innerText = `height=${window.outerHeight}px`;
+}
