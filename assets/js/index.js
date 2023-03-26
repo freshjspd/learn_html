b5d48a8b0cbaa673161753b8fd5011d5c1800ecc
freshjspd <freshjs.pd@gmail.com>
2023-03-26 13:49:06 +0300

    regex example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,10 @@
-/*
 const loginVal = document.querySelector("input[type='text'");
 const passwordVal = document.querySelector("input[type='password'");
 const btn = document.querySelector("input[type='submit'");
 const form = document.querySelector("form");
+
+const regPassword = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/;
+passwordVal.setAttribute('pattern',regPassword.toString());
 
 const data = document.forms[0];
 const singKeys = ['login', 'password'];
@@ -14,37 +16,7 @@ function sendData(event){
     for(let i of singKeys){
         user[i] = data[i].value;
     }
-    
-    //const user = {
-    //    login: loginVal.value,
-    //    password: passwordVal.value
-    //};
-    
     localStorage.setItem('newUser', JSON.stringify(user));
     
 }
-*/
-
-//flags g,m,i
-
-const reg1 = /a+/gm;
-//const reg1 = new RegExp('a+', 'gm');
-let str1 = 'adf aaa as aaaaaaaaa aasd  ddssaAAAA  aasd aa';
-let str2 = 'fffff werwerwer     ewwrwer';
-
-//проверка подходит или нет??
-const isDone = reg1.test(str1);
-console.log(isDone);
-console.log(reg1.test(str2));
-
-//если нужны результаты
-const res1 = str1.match(reg1);
-console.log(res1);
-
-const res2 = [...str1.matchAll(reg1)]
-console.log(res2);
-
-
-
-
 
